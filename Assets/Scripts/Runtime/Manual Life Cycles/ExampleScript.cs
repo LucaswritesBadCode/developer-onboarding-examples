ddcef6cdc2d85a73282d4cb1fe3f99f8dba76140
LucaswritesBadCode <fourschoolguitar@gmail.com>
2026-06-29 17:33:32 +0800

    docs: added comments to Manual Life Cycle scripts

--- Assets/Scripts/Runtime/Manual Life Cycles/ExampleScript.cs
+++ Assets/Scripts/Runtime/Manual Life Cycles/ExampleScript.cs
@@ -1,5 +1,11 @@
 using UnityEngine;
 
+/*all classes should be able to implement:
+Init(): Equivalent to Start()
+OnUpdate(): Equivalent to Update()
+Dispose(): Equivalent to OnDestroy()
+*/
+
 public class ExampleScript1 : MonoBehaviour
 {
     public void Init() { }
--- Assets/Scripts/Runtime/Manual Life Cycles/GameManager.cs
+++ Assets/Scripts/Runtime/Manual Life Cycles/GameManager.cs
@@ -5,6 +5,11 @@ public class GameManager : MonoBehaviour
     public ExampleScript1 script1;
     public ExampleScript2 script2;
 
+    /*in most cases, Start(), Update() (and its variations) and OnDestroy() should only be called in a central GameManager script
+    which then calls the other scripts manually using Init(), OnUpdate() and Dispose().
+    
+    This is done so we have greater control over the order in which each script executes its life cycle functions*/
+
     void Start()
     {
         script1.Init();
