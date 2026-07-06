07841869ec38674ac8606444711c0ef389b303f2
LucaswritesBadCode <fourschoolguitar@gmail.com>
2026-07-06 11:00:34 +0800

    feat: added decoupling examples

--- Assets/Scripts/Runtime/Design Principles/Decoupling.cs
+++ Assets/Scripts/Runtime/Design Principles/Decoupling.cs
@@ -1,16 +1,95 @@
+using System;
 using UnityEngine;
 
-public class Decoupling : MonoBehaviour
+namespace Runtime.DesignPrinciples.Coupling
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class Switch
     {
-        
+        public enum SwitchType { Door, Light, Radio }
+        public SwitchType switchType;
+
+        public Door door;
+        public Light light;
+        public Radio radio;
+
+        public void OnInteract()
+        {
+            switch (switchType)
+            {
+                case SwitchType.Door:
+                    door.OnSwitchPulled();
+                    break;
+                case SwitchType.Light:
+                    light.OnSwitchPulled();
+                    break;
+                case SwitchType.Radio:
+                    radio.OnSwitchPulled();
+                    break;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public class Door
     {
-        
+        public void OnSwitchPulled()
+        {
+            Debug.Log("Door Opened");
+        }
+    }
+
+    public class Light
+    {
+        public void OnSwitchPulled()
+        {
+            Debug.Log("Light On");
+        }
+    }
+    public class Radio
+    {
+        public void OnSwitchPulled()
+        {
+            Debug.Log("Radio Playing");
+        }
+    }
+}
+
+namespace Runtime.DesignPrinciples.Decoupled
+{
+    public interface ISwitchable
+    {
+        void OnSwitchPulled();
+    }
+
+    public class Switch
+    {
+        ISwitchable switchable;
+
+        public void OnInteract()
+        {
+            switchable.OnSwitchPulled();
+        }
+
+        public class Door : ISwitchable
+    {
+        public void OnSwitchPulled()
+        {
+            Debug.Log("Door Opened");
+        }
+    }
+
+    public class Light : ISwitchable
+    {
+        public void OnSwitchPulled()
+        {
+            Debug.Log("Light On");
+        }
+    }
+    public class Radio : ISwitchable
+    {
+        public void OnSwitchPulled()
+        {
+            Debug.Log("Radio Playing");
+        }
+    }
     }
 }
