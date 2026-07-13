830f909bfda2b115f6122878b6d34d5e777de65c
LucaswritesBadCode <fourschoolguitar@gmail.com>
2026-07-13 12:02:30 +0800

    feat: added extra comment in KISS section

--- Assets/Scripts/Runtime/Design Principles/KeepItSimple.cs
+++ Assets/Scripts/Runtime/Design Principles/KeepItSimple.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 public class KeepItSimple
 {
-    /*KISS is predacated on the idea that code should be simple.
+    /* KISS is predacated on the idea that code should be simple.
     Unless there is a very good reason (ie. Optimization, Maintainability), avoid unnecessary complexity in your code 
-    Sometimes, the simplest implementation is the best.*/
+    Sometimes, the simplest implementation is the best. */
 
-    //the following code gets a person's name based on an assigned seat.
+    // the following code gets a person's name based on an assigned seat.
     int seatNumber = 1;
 
-    //Not following KISS
+    // Not following KISS
     public enum PersonName
     {
         Jake = 0, Edward = 1, Colin = 2, Bootsy = 4
@@ -57,5 +57,10 @@ public class KeepItSimple
         return seatOwner;
     }
 
-    //Notice that the simplified function's logic is far easier to follow.
+    // Notice that the simplified function's logic is far easier to follow.
+
+    /* Do note that neither implementation is necessarily the "right" method, 
+    the first implementation is technically more scalable and would work better the larger the set grows.
+    However, if you're sure you won't have hundreds of names to work through, you're better off keeping it simple.
+    Don't do more than you need to unless it brings a major benefit. */
 }
